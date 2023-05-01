142b3ca6732e63638e1665bfd47b43ac393b3423
NickPreddy123 <113132446+NickPreddy123@users.noreply.github.com>
2023-05-01 09:40:53 -0700

    5th commit nicks

--- Assets/Scripts/GoKartTrack.cs
+++ Assets/Scripts/GoKartTrack.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewYork : MonoBehaviour
+public class GoKartTrack : MonoBehaviour
 {
-    public void MapSelectToNewYork()
+    public void MapSelectToGoCart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6);
     }
