86ef6ef4cd41b7583a425d7fe53de7df2cf7dff6
Robbie Gordon <oddballishere@gmail.com>
2023-06-05 10:06:14 -0700

    1212

--- Assets/UpdatedStopwatch.cs
+++ Assets/UpdatedStopwatch.cs
@@ -9,9 +9,7 @@ public class UpdatedStopwatch : MonoBehaviour
     private bool hasStartedLap;
 
     public GameObject lapText;
-    public GameObject lapText2;
-    public GameObject lapText3;
-    public GameObject lapText4;
+    
 
     void Update()
     {
@@ -19,9 +17,7 @@ public class UpdatedStopwatch : MonoBehaviour
         {
             float elapsedTime = Time.time - startTime;
             lapText.GetComponent<Text>().text = "Lap Time: " + elapsedTime.ToString();
-            lapText2.GetComponent<Text>().text = "Lap Time: " + elapsedTime.ToString();
-            lapText3.GetComponent<Text>().text = "Lap Time: " + elapsedTime.ToString();
-            lapText4.GetComponent<Text>().text = "Lap Time: " + elapsedTime.ToString();
+            
         }
     }
 
